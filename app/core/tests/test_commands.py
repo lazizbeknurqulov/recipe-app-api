f97e63243be6fd19435b9b09c37fc90f678b4b89
lazizbeknurqulov <nurqulovlazizbek@gmail.com>
2025-01-04 15:53:51 +0500

    Unused imports deleted

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -4,7 +4,6 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
-
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     
@@ -12,7 +11,6 @@ class CommandTests(SimpleTestCase):
         patched_check.return_value = True
         call_command('wait_for_db')
         patched_check.assert_called_once_with(databases=['default'])
-        
         
     @patch('time.sleep')    
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
