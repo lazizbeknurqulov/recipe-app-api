519a785de739b77492487ebb45aba17b55823835
lazizbeknurqulov <nurqulovlazizbek@gmail.com>
2025-01-04 18:44:57 +0500

    commit

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,16 +1,2 @@
-# """
-# Simple tests
-# """
-# from django.test import SimpleTestCase
 
-
-# from app import calc
-
-# class CalcTests(SimpleTestCase):
-#     "Test the class modeul"
-    
-#     def test_add_numbers(self):
-#         res = calc.add(5, 3)
-        
-#         self.assertEqual(res, 9)
         
