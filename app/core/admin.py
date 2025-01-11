a5160cb7c2a3800b5240551559f12c78cc441dc1
azizabonujuraeva <nurqulovlazizbek@gmail.com>
2025-01-11 14:06:48 +0500

    user edit

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,11 +1,28 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from core import models
 
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields':(
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Import data]es'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
 
 
 
