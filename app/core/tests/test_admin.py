f157a1782b79207b6da38b6634de8716a0862c97
azizabonujuraeva <nurqulovlazizbek@gmail.com>
2025-01-11 13:53:43 +0500

    test_users_listed

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -7,23 +7,32 @@ from django.test import Client
 
 class AdminSiteTest(TestCase):
 
-    def setup(self):
+    def setUp(self):
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
             email='amdin@example.com',
             password='testpass123',
         )
         self.client.force_login(self.admin_user)
+
         self.user = get_user_model().objects.create_user(
             email='user@example.com',
             password='testpass123',
             name='Test User'
         )
+   
+    def test_users_listed(self):
+        url =  reverse('admin:core_user_changelist')
+        res = self.client.get(url)
 
-        def test_users_list(self):
-            url =  reverse('admin:core_user_changelist')
-            res = self.client().get(url)
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
 
-            self.assertContains(res, self.user.name)
-            self.assertContains(res, self.user.email)
-            
+    def test_edit_user_page(self):
+        url = reverse('admin:core_user_change', args=[self.user.id])
+
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
+
+
+
