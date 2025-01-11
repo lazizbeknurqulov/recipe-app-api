f10c184b515998ad2ff33ab3df8cbd0df013fa08
azizabonujuraeva <nurqulovlazizbek@gmail.com>
2025-01-11 23:21:42 +0500

    swagger

--- app/core/admin.py
+++ app/core/admin.py
@@ -23,6 +23,21 @@ class UserAdmin(BaseUserAdmin):
         (_('Import data]es'), {'fields': ('last_login',)}),
     )
     readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+
+            )
+        }),
+    )
 
 
 
